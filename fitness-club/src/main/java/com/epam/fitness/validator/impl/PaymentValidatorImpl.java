2e4fa8f2c9152c91aac4b21ed8f410d3aa65dd88
Aliaksandr Lebiadz <aliaksandr_lebiadz@epam.com>
2019-12-02 13:57:06 +0300

    Refactor the isCardActive method
    Add dateUtils as a dependency to provide the currentDateWithoutTime.

--- fitness-club/src/main/java/com/epam/fitness/validator/impl/PaymentValidatorImpl.java
+++ fitness-club/src/main/java/com/epam/fitness/validator/impl/PaymentValidatorImpl.java
@@ -1,10 +1,11 @@
 package com.epam.fitness.validator.impl;
 
+import com.epam.fitness.utils.DateUtils;
 import com.epam.fitness.validator.api.PaymentValidator;
-import org.apache.commons.lang.time.DateUtils;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -14,6 +15,12 @@ public class PaymentValidatorImpl implements PaymentValidator {
     private static final String EXPIRATION_DATE_REGEXP = "((0[1-9])|(1[0-2]))/\\d{2}";
     private static final String CVV_REGEXP = "\\d{3}";
     private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("MM/yy");
+
+    private DateUtils dateUtils;
+
+    public PaymentValidatorImpl(DateUtils dateUtils){
+        this.dateUtils = dateUtils;
+    }
 
     @Override
     public boolean isCardNumberValid(String cardNumber){
@@ -33,9 +40,8 @@ public class PaymentValidatorImpl implements PaymentValidator {
 
     private boolean isCardActive(String expirationDateStr){
         Date expirationDate = formatDate(expirationDateStr);
-        Date now = new Date();
-        Date truncatedDate = DateUtils.truncate(now, Calendar.MONTH);
-        return !expirationDate.before(truncatedDate);
+        Date currentDateWithoutTime = dateUtils.getCurrentDateWithoutTime();
+        return !expirationDate.before(currentDateWithoutTime);
     }
 
     private Date formatDate(String dateToFormat){
