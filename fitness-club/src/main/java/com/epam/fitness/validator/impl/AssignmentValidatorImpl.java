f2042ae1b5c3607624494c1111f0dff0e6025488
Aliaksandr Lebiadz <aliaksandr_lebiadz@epam.com>
2019-12-03 20:08:30 +0300

    Add DateUtils dependency and refactor

--- fitness-club/src/main/java/com/epam/fitness/validator/impl/AssignmentValidatorImpl.java
+++ fitness-club/src/main/java/com/epam/fitness/validator/impl/AssignmentValidatorImpl.java
@@ -1,9 +1,8 @@
 package com.epam.fitness.validator.impl;
 
+import com.epam.fitness.utils.DateUtils;
 import com.epam.fitness.validator.api.AssignmentValidator;
-import org.apache.commons.lang.time.DateUtils;
 
-import java.util.Calendar;
 import java.util.Date;
 
 public class AssignmentValidatorImpl implements AssignmentValidator {
@@ -12,6 +11,12 @@ public class AssignmentValidatorImpl implements AssignmentValidator {
     private static final int MIN_AMOUNT_OF_SETS = 1;
     private static final int MAX_AMOUNT_OF_REPS = 100;
     private static final int MAX_AMOUNT_OF_SETS = 100;
+
+    private DateUtils dateUtils;
+
+    public AssignmentValidatorImpl(DateUtils dateUtils){
+        this.dateUtils = dateUtils;
+    }
 
     @Override
     public boolean isAmountOfRepsValid(int amount){
@@ -25,9 +30,11 @@ public class AssignmentValidatorImpl implements AssignmentValidator {
 
     @Override
     public boolean isWorkoutDateValid(Date workoutDate){
-        Date now = new Date();
-        Date today = DateUtils.truncate(now, Calendar.DAY_OF_MONTH);
-        return !workoutDate.before(today);
+        return isNotBeforeToday(workoutDate);
     }
 
+    private boolean isNotBeforeToday(Date date){
+        Date today = dateUtils.getCurrentDateWithoutTime();
+        return !date.before(today);
+    }
 }
