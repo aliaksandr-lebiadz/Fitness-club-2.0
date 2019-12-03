d6071c3bf7bf5f11025959fa6f6a74a7729b8e27
Aliaksandr Lebiadz <aliaksandr_lebiadz@epam.com>
2019-12-03 20:09:25 +0300

    Add the getCurrentDateWithoutTime method
    Rename the getCurrentDateWithoutTime method to the getFirstDayOfCurrentMonth

--- fitness-club/src/main/java/com/epam/fitness/utils/DateUtils.java
+++ fitness-club/src/main/java/com/epam/fitness/utils/DateUtils.java
@@ -5,8 +5,13 @@ import java.util.Date;
 
 public class DateUtils {
 
-    public Date getCurrentDateWithoutTime(){
+    public Date getFirstDayOfCurrentMonth(){
         Date now = new Date();
         return org.apache.commons.lang.time.DateUtils.truncate(now, Calendar.MONTH);
+    }
+
+    public Date getCurrentDateWithoutTime(){
+        Date now = new Date();
+        return org.apache.commons.lang.time.DateUtils.truncate(now, Calendar.DAY_OF_MONTH);
     }
 }
