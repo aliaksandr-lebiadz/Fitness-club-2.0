2ba618edec94d433b100e18c6011d6fe22f74f98
Aliaksandr Lebiadz <aliaksandr_lebiadz@epam.com>
2019-12-03 15:17:26 +0300

    Change documentation and and the implementation of some methods

--- fitness-club/src/main/java/com/epam/fitness/dao/impl/common/ExerciseDaoImpl.java
+++ fitness-club/src/main/java/com/epam/fitness/dao/impl/common/ExerciseDaoImpl.java
@@ -1,14 +1,15 @@
-package com.epam.fitness.dao.impl;
+package com.epam.fitness.dao.impl.common;
 
 import com.epam.fitness.builder.Builder;
 import com.epam.fitness.dao.api.ExerciseDao;
+import com.epam.fitness.dao.impl.AbstractDao;
 import com.epam.fitness.entity.assignment.Exercise;
 
 import java.sql.Connection;
 
 /**
  * <p>An implementation of the exercise dao interface to provide
- * an access to the exercise entity in the MySql database.</p>
+ * an access to the exercise entity in the MySql and PostgreSql database.</p>
  *
  * @see Exercise
  */
@@ -21,10 +22,17 @@ public class ExerciseDaoImpl extends AbstractDao<Exercise> implements ExerciseDa
     }
 
     @Override
-    public void save(Exercise entity) {}
-
-    @Override
     protected String getTableName() {
         return EXERCISE_TABLE;
+    }
+
+    @Override
+    protected String getSaveQuery() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    protected Object[] getFields(Exercise exercise) {
+        throw new UnsupportedOperationException();
     }
 }
--- fitness-club/src/main/java/com/epam/fitness/dao/impl/common/GymMembershipDaoImpl.java
+++ fitness-club/src/main/java/com/epam/fitness/dao/impl/common/GymMembershipDaoImpl.java
@@ -1,14 +1,15 @@
-package com.epam.fitness.dao.impl;
+package com.epam.fitness.dao.impl.common;
 
 import com.epam.fitness.builder.Builder;
 import com.epam.fitness.dao.api.GymMembershipDao;
+import com.epam.fitness.dao.impl.AbstractDao;
 import com.epam.fitness.entity.GymMembership;
 
 import java.sql.Connection;
 
 /**
  * <p>An implementation of the gym membership dao interface to provide
- * an access to the gym membership entity in the MySql database.</p>
+ * an access to the gym membership entity in the MySql and PostgreSql database.</p>
  *
  * @see GymMembership
  */
@@ -26,5 +27,15 @@ public class GymMembershipDaoImpl extends AbstractDao<GymMembership> implements 
     @Override
     protected String getTableName(){
         return GYM_MEMBERSHIP_TABLE;
+    }
+
+    @Override
+    protected String getSaveQuery() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    protected Object[] getFields(GymMembership gymMembership) {
+        throw new UnsupportedOperationException();
     }
 }
