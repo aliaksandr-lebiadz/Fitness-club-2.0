42e954f5f63253dfafbb967450deea5fe7475ac1
Aliaksandr Lebiadz <aliaksandr_lebiadz@epam.com>
2019-12-03 20:12:37 +0300

    Add method for the expired date testing, mock's verification

--- fitness-club/src/test/java/com/epam/fitness/validator/PaymentValidatorImplTest.java
+++ fitness-club/src/test/java/com/epam/fitness/validator/PaymentValidatorImplTest.java
@@ -6,12 +6,15 @@ import com.epam.fitness.validator.impl.PaymentValidatorImpl;
 import com.tngtech.java.junit.dataprovider.DataProvider;
 import com.tngtech.java.junit.dataprovider.DataProviderRunner;
 import com.tngtech.java.junit.dataprovider.UseDataProvider;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static org.mockito.Mockito.*;
 
@@ -24,7 +27,8 @@ public class PaymentValidatorImplTest {
     private static final String INVALID_CARD_NUMBER_WITH_LETTERS = "g241351s1012670c";
     private static final String INVALID_CARD_NUMBER_WITH_SIGNS = "!2413.1.101%670?";
 
-    private static final String VALID_EXPIRATION_DATE = "11/19";
+    private static final String NOT_EXPIRED_DATE = "10/19";
+    private static final String EXPIRED_DATE = "09/19";
     private static final String INVALID_EXPIRATION_DATE_BY_NUMBERS = "13/12";
     private static final String INVALID_EXPIRATION_DATE_BY_FORMAT = "11.18";
     private static final String INVALID_EXPIRATION_DATE_BY_LENGTH = "13/121";
@@ -35,10 +39,17 @@ public class PaymentValidatorImplTest {
     private static final String INVALID_CVV_WITH_LETTERS = "8c1";
     private static final String INVALID_CVV_WITH_SIGNS = "!72";
 
-    private static final SimpleDateFormat format = new SimpleDateFormat("MM/yy");
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd/MM/yy");
+    private static final String FIRST_DAY_OF_CURRENT_MONTH = "01/10/19";
 
     private DateUtils dateUtils = mock(DateUtils.class);
     private PaymentValidator validator = new PaymentValidatorImpl(dateUtils);
+
+    @Before
+    public void createMocks() throws ParseException{
+        Date firstDayOfCurrentMonth = DATE_FORMAT.parse(FIRST_DAY_OF_CURRENT_MONTH);
+        when(dateUtils.getFirstDayOfCurrentMonth()).thenReturn(firstDayOfCurrentMonth);
+    }
 
     @DataProvider
     public static Object[][] invalidCardNumberDataProvider(){
@@ -93,15 +104,27 @@ public class PaymentValidatorImplTest {
     }
 
     @Test
-    public void testIsExpirationDateValidShouldReturnTrueWhenValidExpirationDateSupplied() throws ParseException {
+    public void testIsExpirationDateValidShouldReturnTrueWhenNotExpiredDateSupplied() {
         //given
-        when(dateUtils.getCurrentDateWithoutTime()).thenReturn(format.parse("10/19"));
 
         //when
-        boolean actual = validator.isExpirationDateValid(VALID_EXPIRATION_DATE);
+        boolean actual = validator.isExpirationDateValid(NOT_EXPIRED_DATE);
 
         //then
         Assert.assertTrue(actual);
+        verify(dateUtils, times(1)).getFirstDayOfCurrentMonth();
+    }
+
+    @Test
+    public void testIsExpirationDateValidShouldReturnFalseWhenExpiredDateSupplied() {
+        //given
+
+        //when
+        boolean actual = validator.isExpirationDateValid(EXPIRED_DATE);
+
+        //then
+        Assert.assertFalse(actual);
+        verify(dateUtils, times(1)).getFirstDayOfCurrentMonth();
     }
 
     @Test
@@ -137,5 +160,10 @@ public class PaymentValidatorImplTest {
 
         //then
         Assert.assertFalse(actual);
+    }
+
+    @After
+    public void verifyMocks(){
+        verifyNoMoreInteractions(dateUtils);
     }
 }
