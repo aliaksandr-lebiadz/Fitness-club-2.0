a1bc00c0b40778a57bda95c7b6aaa5d49ca2b8c1
Aliaksandr Lebiadz <aliaksandr_lebiadz@epam.com>
2019-12-09 17:34:15 +0300

    Add private methods isRole for readability

--- fitness-club/src/main/java/com/epam/fitness/filter/helper/CommandAccessController.java
+++ fitness-club/src/main/java/com/epam/fitness/filter/helper/CommandAccessController.java
@@ -14,26 +14,38 @@ public class CommandAccessController {
         switch (command){
             case SHOW_CLIENTS_COMMAND:
             case SET_USER_DISCOUNT_COMMAND:
-                return user != null && user.getRole() == UserRole.ADMIN;
+                return isAdmin(user);
 
             case ASSIGN_NUTRITION_TYPE_COMMAND:
             case SHOW_TRAINER_CLIENTS_COMMAND:
-                return user != null && user.getRole() == UserRole.TRAINER;
+                return isTrainer(user);
 
             case GET_MEMBERSHIP_COMMAND:
             case SHOW_ORDER_PAGE_COMMAND:
             case SHOW_ORDERS_COMMAND:
             case SEND_FEEDBACK_COMMAND:
-                return user != null && user.getRole() == UserRole.CLIENT;
+                return isClient(user);
 
             case SHOW_ASSIGNMENTS_COMMAND:
             case CHANGE_ASSIGNMENT_COMMAND:
             case CHANGE_ASSIGNMENT_STATUS_COMMAND:
-                return user != null && (user.getRole() == UserRole.CLIENT || user.getRole() == UserRole.TRAINER);
+                return isClient(user) || isTrainer(user);
 
             default:
                 return true;
         }
+    }
+
+    private boolean isAdmin(User user){
+        return user != null && user.getRole() == UserRole.ADMIN;
+    }
+
+    private boolean isClient(User user){
+        return user != null && user.getRole() == UserRole.CLIENT;
+    }
+
+    private boolean isTrainer(User user){
+        return user != null && user.getRole() == UserRole.TRAINER;
     }
 
 }
