c8b882b04d90ae8716f91433321225835c071aea
Aliaksandr Lebiadz <aliaksandr_lebiadz@epam.com>
2019-12-13 17:18:15 +0300

    Replace OrderBuilder on OrderMapper

--- fitness-club/src/main/java/com/epam/fitness/mapper/OrderMapper.java
+++ fitness-club/src/main/java/com/epam/fitness/mapper/OrderMapper.java
@@ -1,21 +1,17 @@
-package com.epam.fitness.builder.impl;
+package com.epam.fitness.mapper;
 
-import com.epam.fitness.builder.Builder;
 import com.epam.fitness.entity.order.NutritionType;
 import com.epam.fitness.entity.order.Order;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Date;
 
-/**
- * <p>Builds an instance of the {@link Order} class.</p>
- *
- * @see Builder
- * @see Order
- */
-public class OrderBuilder implements Builder<Order> {
+@Component
+public class OrderMapper implements RowMapper<Order> {
 
     private static final String ID_COLUMN = "id";
     private static final String BEGIN_DATE_COLUMN = "begin_date";
@@ -26,15 +22,8 @@ public class OrderBuilder implements Builder<Order> {
     private static final String CLIENT_ID_COLUMN = "client_id";
     private static final String NUTRITION_TYPE_COLUMN = "nutrition_type";
 
-    /**
-     * <p>Builds an instance of the {@link Order} class from
-     * the supplied {@link ResultSet}.</p>
-     *
-     * @param resultSet a result set of parameters
-     * @return a built order
-     */
     @Override
-    public Order build(ResultSet resultSet) throws SQLException {
+    public Order mapRow(ResultSet resultSet, int index) throws SQLException {
         int id = resultSet.getInt(ID_COLUMN);
         int clientId = resultSet.getInt(CLIENT_ID_COLUMN);
         int trainerId = resultSet.getInt(TRAINER_ID_COLUMN);
@@ -55,4 +44,5 @@ public class OrderBuilder implements Builder<Order> {
                 .setNutritionType(nutritionType)
                 .build();
     }
+
 }
