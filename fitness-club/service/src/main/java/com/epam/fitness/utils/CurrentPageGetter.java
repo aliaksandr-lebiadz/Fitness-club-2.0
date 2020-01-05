d8a113b225dce93590375720e9fabd4ca6e652ed
Aliaksandr Lebiadz <Aliaksandr_Lebiadz@epam.com>
2020-01-05 15:44:32 +0300

    Change the getCurrentPage() method

--- fitness-club/service/src/main/java/com/epam/fitness/utils/CurrentPageGetter.java
+++ fitness-club/service/src/main/java/com/epam/fitness/utils/CurrentPageGetter.java
@@ -5,21 +5,10 @@ import javax.servlet.http.HttpServletRequest;
 public class CurrentPageGetter {
 
     private static final String REFERER = "referer";
-    private static final String EMPTY_STRING = "";
-    private static final String SEPARATOR_BETWEEN_SCHEME_AND_SERVER_NAME = "://";
-    private static final String SEPARATOR_BETWEEN_SERVER_NAME_AND_PORT = ":";
-
 
     private CurrentPageGetter() {}
 
     public static String getCurrentPage(HttpServletRequest request){
-        String header = request.getHeader(REFERER);
-        String scheme = request.getScheme();
-        String serverName = request.getServerName();
-        int serverPort = request.getServerPort();
-        String contextPath = request.getContextPath();
-        String serverUrl = scheme + SEPARATOR_BETWEEN_SCHEME_AND_SERVER_NAME + serverName +
-                SEPARATOR_BETWEEN_SERVER_NAME_AND_PORT + serverPort + contextPath;
-        return header.replace(serverUrl, EMPTY_STRING);
+        return request.getHeader(REFERER);
     }
 }
