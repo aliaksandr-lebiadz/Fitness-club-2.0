eb9569ea87b10103dafd236cd8c01dfb726cbf9f
Aliaksandr Lebiadz <Aliaksandr_Lebiadz@epam.com>
2020-01-10 18:36:43 +0300

    Add a constant for error page url

--- fitness-club/web/src/main/java/com/epam/fitness/controller/ControllerAdviceImpl.java
+++ fitness-club/web/src/main/java/com/epam/fitness/controller/ControllerAdviceImpl.java
@@ -6,9 +6,11 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 @ControllerAdvice
 public class ControllerAdviceImpl {
 
+    private static final String ERROR_PAGE_URL = "/error";
+
     @ExceptionHandler(Exception.class)
     public String handleException(Exception ex){
-        return ControllerUtils.createRedirect("/error");
+        return ControllerUtils.createRedirect(ERROR_PAGE_URL);
     }
 
 }
