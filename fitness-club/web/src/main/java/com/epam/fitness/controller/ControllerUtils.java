a6499138454f403266f11d4c79b2130f8986e525
Aliaksandr Lebiadz <Aliaksandr_Lebiadz@epam.com>
2020-01-10 18:33:15 +0300

    Add the getCurrentUser() method

--- fitness-club/web/src/main/java/com/epam/fitness/controller/ControllerUtils.java
+++ fitness-club/web/src/main/java/com/epam/fitness/controller/ControllerUtils.java
@@ -1,11 +1,28 @@
 package com.epam.fitness.controller;
 
+import com.epam.fitness.entity.user.User;
+import com.epam.fitness.exception.ServiceException;
+import com.epam.fitness.service.api.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
 public class ControllerUtils {
 
     private static final String REDIRECT = "redirect:";
     private static final String FORWARD = "forward:";
 
-    private ControllerUtils() {}
+    private UserService userService;
+
+    @Autowired
+    public ControllerUtils(UserService userService) {
+        this.userService = userService;
+    }
 
     public static String createRedirect(String page){
         return REDIRECT + page;
@@ -13,6 +30,18 @@ public class ControllerUtils {
 
     public static String createForward(String page){
         return FORWARD + page;
+    }
+
+    public User getCurrentUser() throws ServiceException {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        String email = userDetails.getUsername();
+        Optional<User> userOptional = userService.findUserByEmail(email);
+        if(userOptional.isPresent()){
+            return userOptional.get();
+        } else{
+            throw new ServiceException("There is no user in the SecurityContext");
+        }
     }
 
 }
