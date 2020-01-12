5b32a779d5683c425addd257a0f26699e395ac0e
Aliaksandr Lebiadz <Aliaksandr_Lebiadz@epam.com>
2020-01-12 21:05:15 +0300

    Add exceptions logging

--- fitness-club/web/src/main/java/com/epam/fitness/controller/ControllerAdviceImpl.java
+++ fitness-club/web/src/main/java/com/epam/fitness/controller/ControllerAdviceImpl.java
@@ -1,15 +1,19 @@
 package com.epam.fitness.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
 @ControllerAdvice
 public class ControllerAdviceImpl {
 
+    private static final Logger LOGGER = LogManager.getLogger(ControllerAdviceImpl.class);
     private static final String ERROR_PAGE_URL = "/error";
 
     @ExceptionHandler(Exception.class)
-    public String handleException(Exception ex){
+    public String handleAnyException(Exception ex){
+        LOGGER.error(ex);
         return ControllerUtils.createRedirect(ERROR_PAGE_URL);
     }
 
