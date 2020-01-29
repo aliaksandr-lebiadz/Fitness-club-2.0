b76c35c87d02429af57e7b6763e0813a0ea6cac8
Aliaksandr Lebiadz <aliaksandr_lebiadz@epam.com>
2020-01-29 19:01:48 +0300

    Change Date to LocalDate and add validation

--- fitness-club/service/src/main/java/com/epam/fitness/entity/AssignmentDto.java
+++ fitness-club/service/src/main/java/com/epam/fitness/entity/AssignmentDto.java
@@ -2,34 +2,41 @@ package com.epam.fitness.entity;
 
 import com.epam.fitness.entity.assignment.AssignmentStatus;
 
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.Min;
 import java.io.Serializable;
-import java.util.Date;
+import java.time.LocalDate;
 
 public class AssignmentDto implements Identifiable, Serializable {
 
     private static final long serialVersionUID = -2057066388628609370L;
 
     private Integer id;
-    private Date workoutDate;
+    @FutureOrPresent
+    private LocalDate workoutDate;
+    @Min(1)
     private int amountOfSets;
+    @Min(1)
     private int amountOfReps;
     private ExerciseDto exercise;
     private AssignmentStatus status;
 
-    public AssignmentDto() {}
+    public AssignmentDto() {
+        this.status = AssignmentStatus.NEW;
+    }
 
     public AssignmentDto(int id, AssignmentStatus status){
         this.id = id;
         this.status = status;
     }
 
-    public AssignmentDto(int amountOfSets, int amountOfReps, Date workoutDate){
+    public AssignmentDto(int amountOfSets, int amountOfReps, LocalDate workoutDate){
         this.amountOfSets = amountOfSets;
         this.amountOfReps = amountOfReps;
         this.workoutDate = workoutDate;
     }
 
-    public AssignmentDto(int id, ExerciseDto exercise, int amountOfSets, int amountOfReps, Date workoutDate){
+    public AssignmentDto(int id, ExerciseDto exercise, int amountOfSets, int amountOfReps, LocalDate workoutDate){
         this(amountOfSets, amountOfReps, workoutDate);
         this.id = id;
         this.exercise = exercise;
@@ -44,15 +51,15 @@ public class AssignmentDto implements Identifiable, Serializable {
         this.id = id;
     }
 
-    public Date getWorkoutDate() {
+    public LocalDate getWorkoutDate() {
         return workoutDate;
     }
 
-    public void setWorkoutDate(Date workoutDate) {
+    public void setWorkoutDate(LocalDate workoutDate) {
         this.workoutDate = workoutDate;
     }
 
-    public int getAmountOfSets() {
+    public Integer getAmountOfSets() {
         return amountOfSets;
     }
 
@@ -60,7 +67,7 @@ public class AssignmentDto implements Identifiable, Serializable {
         this.amountOfSets = amountOfSets;
     }
 
-    public int getAmountOfReps() {
+    public Integer getAmountOfReps() {
         return amountOfReps;
     }
 
