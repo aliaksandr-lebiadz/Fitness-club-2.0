edd197e387b899d60de38f8d66b8b862a665a4ba
Aliaksandr Lebiadz <aliaksandr_lebiadz@epam.com>
2020-01-30 18:37:44 +0300

    Change method signature

--- fitness-club/service/src/main/java/com/epam/fitness/service/api/OrderService.java
+++ fitness-club/service/src/main/java/com/epam/fitness/service/api/OrderService.java
@@ -3,7 +3,6 @@ package com.epam.fitness.service.api;
 
 import com.epam.fitness.entity.GymMembershipDto;
 import com.epam.fitness.entity.OrderDto;
-import com.epam.fitness.entity.UserDto;
 import com.epam.fitness.exception.ServiceException;
 
 import javax.transaction.Transactional;
@@ -15,7 +14,8 @@ public interface OrderService {
     void create(int clientId, GymMembershipDto gymMembershipDto) throws ServiceException;
     @Transactional
     void updateById(int id, OrderDto order) throws ServiceException;
-    List<OrderDto> getOrdersOfTrainerClient(int clientId, UserDto trainer);
+    @Transactional
+    List<OrderDto> getOrdersOfTrainerClient(int clientId, int trainerId) throws ServiceException;
     @Transactional
     List<OrderDto> getOrdersByClientId(int clientId) throws ServiceException;
     OrderDto getById(int id) throws ServiceException;
