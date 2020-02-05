72a8d5852c5f4e0f5b44c2d5a9669f7e94aeddc7
Aliaksandr Lebiadz <aliaksandr_lebiadz@epam.com>
2020-02-05 20:00:59 +0300

    Add error page routing

--- fitness-club/frontend/src/App.js
+++ fitness-club/frontend/src/App.js
@@ -4,6 +4,7 @@ import { Switch } from 'react-router-dom';
 import UsersTable from './component/user/UsersTable';
 import Home from './component/home/Home';
 import Header from './component/header/Header';
+import Error from './component/error/Error';
 import { Route } from 'react-router-dom';
 
 class App extends React.Component {
@@ -14,6 +15,7 @@ class App extends React.Component {
           <Switch>
             <Route exact path='/' component={Home}/>
             <Route exact path='/users' component={UsersTable}/>
+            <Route exact path='/error' component={Error}/>
           </Switch>
         </main>
     );
