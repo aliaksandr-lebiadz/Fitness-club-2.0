0830f4768b8af69c64f3a79cec488354e2b262a2
Aliaksandr Lebiadz <aliaksandr_lebiadz@epam.com>
2020-02-05 20:00:22 +0300

    Add error page and styles for the page

---
+++ fitness-club/frontend/src/component/error/Error.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../styles/Error.css';
+import { history } from '../../helpers';
+
+class Error extends React.Component{
+    render(){
+        return(
+            <div id="error-intro">
+                <div id="error">
+                    <span id="error-text">Something went wrong...</span>
+                    <button className="custom-button" id="home-button" onClick={() => history.push('/')}>Home</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Error;
