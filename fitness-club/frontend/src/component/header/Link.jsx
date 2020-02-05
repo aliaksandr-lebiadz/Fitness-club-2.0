dfb50fbfdaf265a1de5f173dfcac1c59e7ae2e48
Aliaksandr Lebiadz <aliaksandr_lebiadz@epam.com>
2020-02-05 17:16:06 +0300

    Add Link component

---
+++ fitness-club/frontend/src/component/header/Link.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Link extends React.Component{
+    render(){
+        const { value, url } = this.props;
+        return(
+            <a class="navigation_link simple" href={url}>{value}</a>
+        );
+    }
+}
+
+export default Link;
