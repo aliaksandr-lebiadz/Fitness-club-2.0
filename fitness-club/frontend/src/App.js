ab0ad0a130b453c475ac94b2eef0d82d8eb888b4
Aliaksandr Lebiadz <Aliaksandr_Lebiadz@epam.com>
2020-02-08 21:31:37 +0300

    Add current user state and change route of empty path

--- fitness-club/frontend/src/App.js
+++ fitness-club/frontend/src/App.js
@@ -1,25 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { Switch } from "react-router-dom";
 import UsersTable from "./component/user/UsersTable";
+import Login from "./component/login/Login";
 import Home from "./component/home/Home";
 import Header from "./component/header/Header";
 import Error from "./component/error/Error";
 import { Route } from "react-router-dom";
 
-class App extends React.Component {
-  render() {
-    return (
-      <main>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/users" component={UsersTable} />
-          <Route exact path="/error" component={Error} />
-        </Switch>
-      </main>
-    );
-  }
-}
-
+const App = () => {
+  const [currentUser, setCurrentUser] = useState(null);
+  return (
+    <main>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          {currentUser == null ? (
+            <Login setCurrentUser={setCurrentUser} />
+          ) : (
+            <Home />
+          )}
+        </Route>
+        <Route exact path="/users" component={UsersTable} />
+        <Route exact path="/error" component={Error} />
+      </Switch>
+    </main>
+  );
+};
 export default App;
