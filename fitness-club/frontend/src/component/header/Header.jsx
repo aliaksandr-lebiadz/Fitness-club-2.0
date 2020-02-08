5a21881a37914e83b975932fd85d97635e11846c
Aliaksandr Lebiadz <Aliaksandr_Lebiadz@epam.com>
2020-02-08 21:29:13 +0300

    Change the color of header

--- fitness-club/frontend/src/component/header/Header.jsx
+++ fitness-club/frontend/src/component/header/Header.jsx
@@ -1,21 +1,26 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import AppBar from '@material-ui/core/AppBar';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import AppBar from "@material-ui/core/AppBar";
 
 const useStyles = makeStyles({
-    logo: {
-        padding: 20
-    }
-})
+  header: {
+    backgroundColor: "#76FEC5"
+  },
+  logo: {
+    padding: 20
+  }
+});
 
 const Header = () => {
-    const classes = useStyles();
-    return(
-        <AppBar position='fixed' size='med'>
-            <Typography className={classes.logo} variant='h6'>Fitness club</Typography>
-        </AppBar>
-    );
-}
+  const classes = useStyles();
+  return (
+    <AppBar className={classes.header} position="fixed">
+      <Typography className={classes.logo} variant="h6" color="textPrimary">
+        Fitness club
+      </Typography>
+    </AppBar>
+  );
+};
 
 export default Header;
