0c873c996382a4898c0b0cd73ce05f5b85a555f4
Aliaksandr Lebiadz <Aliaksandr_Lebiadz@epam.com>
2020-02-08 14:00:41 +0300

    Refactor some minor things

--- fitness-club/frontend/src/App.js
+++ fitness-club/frontend/src/App.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import './App.css'
-import { Switch } from 'react-router-dom';
-import UsersTable from './component/user/UsersTable';
-import Home from './component/home/Home';
-import Header from './component/header/Header';
-import Error from './component/error/Error';
-import { Route } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import { Switch } from "react-router-dom";
+import UsersTable from "./component/user/UsersTable";
+import Home from "./component/home/Home";
+import Header from "./component/header/Header";
+import Error from "./component/error/Error";
+import { Route } from "react-router-dom";
 
 class App extends React.Component {
-  render(){
+  render() {
     return (
-        <main>
-          <Header />
-          <Switch>
-            <Route exact path='/' component={Home}/>
-            <Route exact path='/users' component={UsersTable}/>
-            <Route exact path='/error' component={Error}/>
-          </Switch>
-        </main>
+      <main>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/users" component={UsersTable} />
+          <Route exact path="/error" component={Error} />
+        </Switch>
+      </main>
     );
   }
 }
- 
+
 export default App;
