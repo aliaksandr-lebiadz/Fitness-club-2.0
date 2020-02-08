f36958d7a7b75342957acdc066ce91faf229aaac
Aliaksandr Lebiadz <Aliaksandr_Lebiadz@epam.com>
2020-02-08 21:29:42 +0300

    Add login function

--- fitness-club/frontend/src/service/UserService.js
+++ fitness-club/frontend/src/service/UserService.js
@@ -1,31 +1,36 @@
-import axios from 'axios';
+import axios from "axios";
 
-const USERS_URL = '/users';
+const USERS_URL = "/users";
 
 export const userService = {
-    getAll,
-    deleteById,
-    updateById,
-    create,
-    getById
+  getAll,
+  deleteById,
+  updateById,
+  create,
+  getById,
+  login
 };
 
-function getAll(){
-    return axios.get(USERS_URL);
+function getAll() {
+  return axios.get(USERS_URL);
 }
 
-function deleteById(id){
-    return axios.delete(USERS_URL + '/' + id);
+function deleteById(id) {
+  return axios.delete(USERS_URL + "/" + id);
 }
 
-function updateById(id, user){
-    return axios.patch(USERS_URL + '/' + id, user);
+function updateById(id, user) {
+  return axios.patch(USERS_URL + "/" + id, user);
 }
 
-function create(user){
-    return axios.post(USERS_URL, user);
+function create(user) {
+  return axios.post(USERS_URL, user);
 }
 
-function getById(id){
-    return axios.get(USERS_URL + '/' + id);
+function getById(id) {
+  return axios.get(USERS_URL + "/" + id);
+}
+
+function login(email, password) {
+  return axios.post("/doLogin", { email, password });
 }
