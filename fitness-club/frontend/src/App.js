517ec7b26f49248d5b7339c5e2dd7360032e2fff
Aliaksandr Lebiadz <aliaksandr_lebiadz@epam.com>
2020-02-11 15:58:18 +0300

    Delete current user state

--- fitness-club/frontend/src/App.js
+++ fitness-club/frontend/src/App.js
@@ -1,30 +1,25 @@
-import React, { useState } from "react";
+import React from "react";
+import { Router, Switch, Route } from "react-router";
 import "./App.css";
-import { Switch } from "react-router-dom";
-import UsersTable from "./component/user/UsersTable";
-import Login from "./component/login/Login";
-import Home from "./component/home/Home";
+import { history } from "./helpers/history";
 import Header from "./component/header/Header";
+import Login from "./component/login/Login";
+import UsersTable from "./component/user/UsersTable";
 import Error from "./component/error/Error";
-import { Route } from "react-router-dom";
 
 const App = () => {
-  const [currentUser, setCurrentUser] = useState(null);
   return (
     <main>
-      <Header />
-      <Switch>
-        <Route exact path="/">
-          {currentUser == null ? (
-            <Login setCurrentUser={setCurrentUser} />
-          ) : (
-            <Home />
-          )}
-        </Route>
-        <Route exact path="/users" component={UsersTable} />
-        <Route exact path="/error" component={Error} />
-      </Switch>
+      <Router history={history}>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/users" component={UsersTable} />
+          <Route exact path="/error" component={Error} />
+        </Switch>
+      </Router>
     </main>
   );
 };
+
 export default App;
