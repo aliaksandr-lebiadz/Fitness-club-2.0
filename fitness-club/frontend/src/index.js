c9cc41580da3cd8ae51e0c4a7cae3ba3d224081c
Aliaksandr Lebiadz <aliaksandr_lebiadz@epam.com>
2020-02-11 15:56:02 +0300

    Add redux store creation

--- fitness-club/frontend/src/index.js
+++ fitness-club/frontend/src/index.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App'
-import * as serviceWorker from './serviceWorker';
-import { history } from './helpers';
-import { Router } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
+import storeFactory from "./store";
+import App from "./App";
+import sampleData from "./initialState";
+
+const initialState = localStorage["redux-store"]
+  ? JSON.parse(localStorage["redux-store"])
+  : sampleData;
+
+const saveState = () => {
+  localStorage["redux-store"] = JSON.stringify(store.getState());
+};
+
+const store = storeFactory(initialState);
+store.subscribe(saveState);
+
+window.React = React;
+window.store = store;
 
 ReactDOM.render(
-    <Router history={history}>
-        <App />
-    </Router>,
-    document.getElementById('root'));
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 serviceWorker.unregister();
