e71ec72e7c5604e7f277a2848c92f3e91c045fa5
Aliaksandr Lebiadz <aliaksandr_lebiadz@epam.com>
2020-02-11 15:55:21 +0300

    Create utils for redux to apply middleware

---
+++ fitness-club/frontend/src/store/index.js
@@ -0,0 +1,7 @@
+import rootReducer from "./reducers";
+import thunk from "redux-thunk";
+import { createStore, applyMiddleware } from "redux";
+
+export default (initialState = {}) => {
+  return applyMiddleware(thunk)(createStore)(rootReducer, initialState);
+};
