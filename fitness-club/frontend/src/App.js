4cf0c08705330046c5ea917ab2231f1d0bf27b0f
Aliaksandr Lebiadz <aliaksandr_lebiadz@epam.com>
2020-02-11 20:20:51 +0300

    Add urls access security

--- fitness-club/frontend/src/App.js
+++ fitness-club/frontend/src/App.js
@@ -4,22 +4,40 @@ import "./App.css";
 import { history } from "./helpers/history";
 import Header from "./component/header/Header";
 import Login from "./component/login/Login";
+import Home from "./component/home/Home";
 import UsersTable from "./component/user/UsersTable";
 import Error from "./component/error/Error";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
-const App = () => {
+const App = props => {
+  const { currentUser } = props;
+  const role = currentUser !== null ? currentUser.role : null;
+
   return (
     <main>
       <Router history={history}>
         <Header />
         <Switch>
-          <Route exact path="/" component={Login} />
-          <Route exact path="/users" component={UsersTable} />
+          <Route exact path="/users">
+            {role === "ADMIN" ? <UsersTable /> : <Error />}
+          </Route>
           <Route exact path="/error" component={Error} />
+          <Route exact path="/**">
+            {currentUser !== null ? <Home /> : <Login />}
+          </Route>
         </Switch>
       </Router>
     </main>
   );
 };
 
-export default App;
+App.propTypes = {
+  currentUser: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  currentUser: state.currentUser
+});
+
+export default connect(mapStateToProps, null)(App);
