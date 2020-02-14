a1218d3e2528b82924be174784635478afd50b06
Aliaksandr Lebiadz <aliaksandr_lebiadz@epam.com>
2020-02-14 17:14:28 +0300

    Add customized constraint messages handling

--- fitness-club/web/src/main/java/com/epam/fitness/exception/controller/ControllerAdviceImpl.java
+++ fitness-club/web/src/main/java/com/epam/fitness/exception/controller/ControllerAdviceImpl.java
@@ -5,24 +5,36 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 @RestControllerAdvice
 public class ControllerAdviceImpl extends ResponseEntityExceptionHandler {
 
     private static final Logger LOGGER = LogManager.getLogger(ControllerAdviceImpl.class);
-    private static final String INVALID_METHOD_ARGUMENTS_MESSAGE = "Invalid method parameter!";
+    private static final String ERROR_MESSAGE_FORMAT = "%s %s, but was: %s";
 
     @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exception,
                                                                   HttpHeaders headers,
                                                                   HttpStatus status,
                                                                   WebRequest request) {
-        ApiError apiError = new ApiError(INVALID_METHOD_ARGUMENTS_MESSAGE);
+        LOGGER.error(exception);
+
+        BindingResult bindingResult = exception.getBindingResult();
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        List<String> errorsMessages = getMessagesFromFieldsErrors(fieldErrors);
+        ApiError apiError = new ApiError(errorsMessages);
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(apiError);
@@ -33,10 +45,18 @@ public class ControllerAdviceImpl extends ResponseEntityExceptionHandler {
         LOGGER.error(exception);
 
         String message = exception.getMessage();
-        ApiError apiError = new ApiError(message);
+        ApiError apiError = new ApiError(Collections.singletonList(message));
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(apiError);
+    }
+
+    private List<String> getMessagesFromFieldsErrors(List<FieldError> fieldErrors){
+        Function<FieldError, String> formatter =
+                (f) -> String.format(ERROR_MESSAGE_FORMAT, f.getField(), f.getDefaultMessage(), f.getRejectedValue());
+        return fieldErrors.stream()
+                .map(formatter)
+                .collect(Collectors.toList());
     }
 
 }
